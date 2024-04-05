ff1c3013417625e85a66d6c39bd440c51899500f
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2024-04-05 20:34:05 +0530

    Add in GitHub

---
+++ script.js
@@ -0,0 +1,17 @@
+const access_key = "gvQVJC2sQZzoUONRpLbVmNCX36V1cMKNcjBnM64LinQ";
+const form = document.querySelector("form");
+const input = document.querySelector("#input");
+const moreBtn = document.querySelector(".search-images-btn");
+const imageContainer = document.querySelector(".search-images");
+const SearchImage = async(e)=>{
+    e.preventDefault();
+    const imgData = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${input.value}&client_id=${access_key}&per_page=12`)
+    const imgObj = await imgData.json();
+    console.log(imgObj);
+    const arr = imgObj.results;
+    arr.forEach((el,index) => {
+        imageContainer.innerHTML += `<img class="h-60 m-2" src="${imgObj.results[index].urls.full }"/>`
+    });
+    // moreBtn.innerHTML += (`<button class="bg-red-400 p-2 w-32">More</button>`);
+}
+form.addEventListener("submit",SearchImage);
