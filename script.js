ee244cb892c22d7cbafd6431a33a501e2813d220
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2024-04-06 08:15:16 +0530

    input.value = '' when name search

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ const input = document.querySelector("#input");
 const moreBtn = document.querySelector(".search-images-btn");
 const imageContainer = document.querySelector(".search-images");
 const SearchImage = async(e)=>{
+    imageContainer.innerHTML = '';
     e.preventDefault();
     const imgData = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${input.value}&client_id=${access_key}&per_page=12`)
     const imgObj = await imgData.json();
     console.log(imgObj);
     const arr = imgObj.results;
     arr.forEach((el,index) => {
-        imageContainer.innerHTML += `<img class="h-60 m-2" src="${imgObj.results[index].urls.full }"/>`
+        imageContainer.innerHTML += `<img class="h-60 m-2" src="${imgObj.results[index].urls.full}"/>`
     });
+    input.value = '';
     // moreBtn.innerHTML += (`<button class="bg-red-400 p-2 w-32">More</button>`);
 }
 form.addEventListener("submit",SearchImage);
